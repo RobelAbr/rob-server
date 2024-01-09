1a7757acbdb6b4605b7b6e11ffef677eca8d5f6b
RobelAbr <robel.a@hotmail.de>
2024-01-09 13:21:36 +0100

    Refactored the homeHandler to use an html template to render

--- main.go
+++ main.go
@@ -3,19 +3,37 @@ package main
 import (
 	"fmt"
 	"github.com/go-chi/chi/v5"
+	"html/template"
+	"log"
 	"net/http"
+	"path/filepath"
 )
 
+// commit something
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+	tplPath := filepath.Join("templates", "home.gohtml")
+	tpl, err := template.ParseFiles(tplPath)
+	if err != nil {
+		log.Printf("parsing template: %v", err)
+		http.Error(w, "There was a error parsing the template.", http.StatusInternalServerError)
+		return
+	}
+	err = tpl.Execute(w, nil)
+	if err != nil {
+		log.Printf("executing template: %v", err)
+		http.Error(w, "There was a error executing the template.", http.StatusInternalServerError)
+		return
+	}
 }
 
+// commit something
 func contactHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:rabraha@hotmail.io\">rabraha@hotmail.io</a>.")
 }
 
+// commit something
 func faqHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, `<b><h1>FAQ Page</h1><p>Welcome for any Questions:<br /></b>
@@ -26,13 +44,15 @@ func faqHandler(w http.ResponseWriter, r *http.Request) {
 </ul>
 `)
 }
+
+// all pages of the webserver that be called
 func main() {
 	r := chi.NewRouter()
 	r.Get("/", homeHandler)
 	r.Get("/contact", contactHandler)
 	r.Get("/faq", faqHandler)
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
-		http.Error(w, "<h1>Page not found<h1>", http.StatusNotFound)
+		http.Error(w, "Ohhh sorry....Page not found", http.StatusNotFound)
 	})
 	fmt.Println("Starting the server on :3000...")
 	http.ListenAndServe(":3000", r)
