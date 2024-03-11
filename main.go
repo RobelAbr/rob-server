7574a2f75ef6e9ccb9c6277c0c70af390b4b6376
RobelAbr <robel.a@hotmail.de>
2024-03-11 15:08:05 +0100

    add comments to main and template function

--- main.go
+++ main.go
@@ -9,94 +9,94 @@ import (
 	"path/filepath"
 )
 
-// executeTemplate is a utility function to render an HTML template.
-// It takes an http.ResponseWriter and the filepath of the template as input.
+// executeTemplate ist eine Hilfsfunktion zum Rendern eines HTML-Templates.
+// Sie nimmt einen http.ResponseWriter und den Dateipfad des Templates als Eingabe.
 func executeTemplate(w http.ResponseWriter, filepath string) {
-	// Set the Content-Type header to indicate that the response is HTML.
+	// Setze den Content-Type-Header, um anzuzeigen, dass die Antwort HTML ist.
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 
-	// Parse the HTML template located at the specified filepath.
+	// Parsen des HTML-Templates am angegebenen Dateipfad.
 	tpl, err := template.ParseFiles(filepath)
 	if err != nil {
-		// If there is an error parsing the template, log the error,
-		// send an internal server error response, and return from the function.
-		log.Printf("parsing template: %v", err)
-		http.Error(w, "There was a error parsing the template.", http.StatusInternalServerError)
+		// Bei einem Fehler beim Parsen des Templates: Fehler protokollieren,
+		// eine interne Serverfehlerantwort senden und die Funktion beenden.
+		log.Printf("Template-Parsing-Fehler: %v", err)
+		http.Error(w, "Beim Parsen des Templates ist ein Fehler aufgetreten.", http.StatusInternalServerError)
 		return
 	}
 
-	// Execute the parsed template, writing the output to the http.ResponseWriter.
-	// Pass nil as the data since we are not providing any specific data to the template.
+	// Ausführen des geparsen Templates und Schreiben des Ergebnisses in den http.ResponseWriter.
+	// Nil wird als Daten übergeben, da wir keine spezifischen Daten an das Template liefern.
 	err = tpl.Execute(w, nil)
 	if err != nil {
-		// If there is an error executing the template, log the error,
-		// send an internal server error response, and return from the function.
-		log.Printf("executing template: %v", err)
-		http.Error(w, "There was a error executing the template.", http.StatusInternalServerError)
+		// Bei einem Fehler beim Ausführen des Templates: Fehler protokollieren,
+		// eine interne Serverfehlerantwort senden und die Funktion beenden.
+		log.Printf("Template-Ausführungsfehler: %v", err)
+		http.Error(w, "Beim Ausführen des Templates ist ein Fehler aufgetreten.", http.StatusInternalServerError)
 		return
 	}
 }
 
-// here we call the template home
+// homeHandler ruft das Template "home" auf
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	tplPath := filepath.Join("templates", "home.gohtml")
 	executeTemplate(w, tplPath)
 }
 
-// here we call the template contact
+// contactHandler ruft das Template "contact" auf
 func contactHandler(w http.ResponseWriter, r *http.Request) {
 	tplPath := filepath.Join("templates", "contact.gohtml")
 	executeTemplate(w, tplPath)
 }
 
-// here we call the template career
+// careerHandler ruft das Template "career" auf
 func careerHandler(w http.ResponseWriter, r *http.Request) {
 	tplPath := filepath.Join("templates", "career.gohtml")
 	executeTemplate(w, tplPath)
 }
 
-// commit something
+// faqHandler ruft das Template "faq" auf
 func faqHandler(w http.ResponseWriter, r *http.Request) {
 	executeTemplate(w, filepath.Join("templates", "faq.gohtml"))
 }
 
-// here we call the template login
+// loginHandler ruft das Template "login" auf
 func loginHandler(w http.ResponseWriter, r *http.Request) {
 	tplPath := filepath.Join("templates", "login.gohtml")
 	executeTemplate(w, tplPath)
 }
 
-// here we call the template register
+// registerHandler ruft das Template "register" auf
 func registerHandler(w http.ResponseWriter, r *http.Request) {
 	tplPath := filepath.Join("templates", "register.gohtml")
 	executeTemplate(w, tplPath)
 }
 
-// here we call the template tutorials
+// tutorialsHandler ruft das Template "tutorials" auf
 func tutorialsHandler(w http.ResponseWriter, r *http.Request) {
 	tplPath := filepath.Join("templates", "tutorials.gohtml")
 	executeTemplate(w, tplPath)
 }
 
-// here we call the template tutorials
+// developHandler ruft das Template "develop" auf
 func developHandler(w http.ResponseWriter, r *http.Request) {
 	tplPath := filepath.Join("templates", "develop.gohtml")
 	executeTemplate(w, tplPath)
 }
 
-// here we call the template tutorials
+// learnHandler ruft das Template "learn" auf
 func learnHandler(w http.ResponseWriter, r *http.Request) {
 	tplPath := filepath.Join("templates", "learn.gohtml")
 	executeTemplate(w, tplPath)
 }
 
-// here we call the template tutorials
+// materialHandler ruft das Template "material" auf
 func materialHandler(w http.ResponseWriter, r *http.Request) {
 	tplPath := filepath.Join("templates", "material.gohtml")
 	executeTemplate(w, tplPath)
 }
 
-// starts server and all pages of the webserver that be called
+// main startet den Server und ruft alle Seiten des Webservers auf, die aufgerufen werden können
 func main() {
 	r := chi.NewRouter()
 	r.Get("/", homeHandler)
@@ -113,6 +113,6 @@ func main() {
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Ohhh sorry....Page not found", http.StatusNotFound)
 	})
-	fmt.Println("Starting the server on :3000...")
+	fmt.Println("Server wird auf :3000 gestartet...")
 	http.ListenAndServe(":3000", r)
 }
